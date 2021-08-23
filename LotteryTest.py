07d9b7a3463bfc470e974bc1830510922627d7ea
Clarencechang3 <80869122+Clarencechang3@users.noreply.github.com>
2021-08-22 23:51:00 -0400

    Add files via upload
    Added a plot - Still need to add a live plot and possibly a plot that shows incremental increase of cost vs profit

--- LotteryTest.py
+++ LotteryTest.py
@@ -1,11 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Aug  6 04:25:02 2021
-
-@author: clarencechang
-"""
-
 # lottery game where number choices are between 1-49; your given a combination of 6 numbers to find
 # your allowed up to 6 numbers for your inital purchase price, and a marginal increase per number 
 # find the expected cost per marginal number (past six ), and the expected cost for the first six 
@@ -13,6 +5,9 @@ Created on Fri Aug  6 04:25:02 2021
 import math
 import random
 import sys 
+import matplotlib.pyplot as plt
+from itertools import count
+from matplotlib.animation import FuncAnimation
 
 sys.setrecursionlimit(10 ** 6)
 
@@ -22,6 +17,7 @@ choices_l = int(input("Last Number ")) # Second Number (in this case 49)
 numberofcomb = int(input("Number of numbers in a winning combination ")) 
 Lottery_payout = int(input("Winning Amount ")) 
 Simulations = int(input("Simulations for Efficancy Testing ")) # Amount of simulations to test expected cost
+cost = int(input("Cost Per Ticket ")) # Cost per ticket to test efficancy 
 
 total_combinations = (math.factorial(choices_l)) / ((math.factorial(numberofcomb) * (math.factorial(choices_l - numberofcomb))))
 
@@ -75,15 +71,19 @@ def test(guess_storage):
 
 total_profit = 0
  
+profit_vis = []
+time_vis = []
+counter = count()
 
 for x in range(1,Simulations + 1 ):
 
     
     
+    
     key_store = (random.sample(range(choices_b,choices_l + 1 ), numberofcomb ))
     sorted_key = sorted(key_store)
     
-    cost =  price_total  #adjust for winning odds; this shoudl always win though or break even 
+   
    
            
     
@@ -93,6 +93,14 @@ for x in range(1,Simulations + 1 ):
     profit = (Lottery_payout - cost_sum)
         # print(f' this is the current count {count}, cost_sum {cost_sum}')
         
+
+    time_vis.append(next(counter))
+    profit_vis.append(profit)
+    plt.plot(time_vis, profit_vis)
+       
+    plt.xlabel(" Time in Intervals of 1")
+    plt.ylabel("Profit in Intervals of 1")
+    
     total_profit += profit 
     average_cost = total_profit / Simulations 
         
@@ -108,6 +116,20 @@ for x in range(1,Simulations + 1 ):
             
     print (f' total profit is {profit}')
     
-print (f"There is a total of {total_combinations} combinations, the efficient pricing per number is {price_per},\n a {numberofcomb} digit combination should cost {price_total}, on {Simulations} simulations your total profit is {total_profit}, with an average cost of {average_cost}" )
+    
+    
 
-  
+
+
+# def Visual(data):
+#       global profit 
+# #     time_vis.append(next(counter))
+#       profit_vis.append(profit)
+# #     plt.plot(time_vis, profit_vis)
+#       print (profit_vis)
+# # Visualize = FuncAnimation(plt.gcf(), Visual, interval = 1000)
+    
+    
+# Visual(profit)
+    
+print (f"There is a total of {total_combinations} combinations, the efficient pricing per number is {price_per},\n a {numberofcomb} digit combination should cost {price_total}, on {Simulations} simulations your total profit is {total_profit}, with an average cost of {average_cost}" )
